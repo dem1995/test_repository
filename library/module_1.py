a2901edc457f39580ff59721aaa34f962e315f80
Dawn McKnight <demcknig@ualberta.ca>
2024-08-19 04:04:01 -0600

    adding docstrings

--- library/module_1.py
+++ library/module_1.py
@@ -1,3 +1,15 @@
 class Adder:
-    def add(self, a, b):
+    """A simple class to add two numbers."""
+
+    def add(self, a: float, b: float) -> float:
+        """
+        Add two numbers.
+
+        :param a: The first number.
+        :param b: The second number.
+        :type a: float
+        :type b: float
+        :return: The sum of the two numbers.
+        :rtype: float
+        """
         return a + b
